84a5228d3ca1de6fd9b8b5e3066601be8a51559c
Bora Yüret <bora.yuret@bilgeadam.com>
2021-03-06 13:56:21 +0300

    cevir.py dosyasına sıcaklıkların birbirine çevrilmesi eklendi.

---
+++ cevir.py
@@ -0,0 +1,11 @@
+def kelvin_to_celcius(k_temp):
+    return k_temp - 273.15
+
+def celcius_to_kelvin(c_temp):
+    return c_temp + 273.15
+
+def fahrenheit_to_celcius(f_temp):
+    return (f_temp-32)/1.8
+
+def celcius_to_fahrenheit(c_temp):
+    return (c_temp * 1.8) + 32
